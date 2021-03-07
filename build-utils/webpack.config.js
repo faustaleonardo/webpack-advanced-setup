89c75a449db4be90e2886b9328f8aa5e9a38ef13
faustaleonardo <faustaleonardo11@gmail.com>
2021-03-07 14:54:49 +0700

    setup addons and added plugin: webpack-bundle-analyzer

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -1,7 +1,15 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
-module.exports = ({ env }) => {
+const getAddons = (addonsArg) => {
+  const addons = Array.isArray(addonsArg) ? addonsArg : [addonsArg];
+
+  return addons
+    .filter(Boolean)
+    .map((name) => require(`./addons/webpack.${name}.js`));
+};
+
+module.exports = ({ env, addon }) => {
   const envConfig = require(`./webpack.${env}.js`);
-  return merge(commonConfig, envConfig);
+  return merge(commonConfig, envConfig, ...getAddons(addon));
 };
