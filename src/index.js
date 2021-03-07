daac7fa652ba827bd8ffc911ee36a6f6bd413470
faustaleonardo <faustaleonardo11@gmail.com>
2021-03-07 13:42:00 +0700

    added devtool: source map for tracking bugs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import sum from './sum';
+
 const person = 'fausta';
 console.log(`Hello World, from ${person}!`);
+
+console.log(sum(1, 2));
---
+++ src/sum.js
@@ -0,0 +1,4 @@
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__['default'] = function (a, b) {
+  return a + c;
+};
